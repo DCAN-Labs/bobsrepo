62eb4f0f655dc4517d3f0583912941d1aab259e5
LuciMoore <lucille.a.mo@gmail.com>
2024-03-07 15:38:23 -0600

    updated template code to use default for skullstripping instead of feeding in  external brainmask. updated resources file with more informative jobname

--- processing/s3_wrappers/BCP/ext_aseg/resources_abcd-hcp-pipeline_infant.sh
+++ processing/s3_wrappers/BCP/ext_aseg/resources_abcd-hcp-pipeline_infant.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
-#SBATCH -J albertXaseg
+#SBATCH -J BCPXaseg
 #SBATCH --mem=60gb
 #SBATCH --tmp=100gb
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=lmoore@umn.edu
+#SBATCH --mail-user=fayzu001@umn.edu
 #SBATCH -o output_logs/abcd-hcp-pipeline_infant_%A_%a.out
 #SBATCH -e output_logs/abcd-hcp-pipeline_infant_%A_%a.err
 #SBATCH -A feczk001
@@ -15,6 +15,7 @@
 cd run_files.abcd-hcp-pipeline_infant
 
 module load singularity
+#module load fsl
 
 file=run${SLURM_ARRAY_TASK_ID}
 
